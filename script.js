ad07afc7aaa4bf1718373e6e8f1b9dfeab00bbb9
Wildkratts6 <senumiled@gmail.com>
2025-07-07 15:06:31 -0500

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function pressMe() {
   alert("You pressed the button! Great job!");
 }
+
+document.body.addEventListener("click", () => {
+  const colors = ["lightblue", "lightgreen", "lavender", "lightpink", "peachpuff", "lightyellow"];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
+});
